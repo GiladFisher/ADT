e60e618351f0ad33dddfe5f4a7096a740f7c76a1
GiladFisher <giladfisher98@gmail.com>
2023-03-16 18:12:02 +0200

    Update AdptArray.c

--- AdptArray.c
+++ AdptArray.c
@@ -1,54 +1,55 @@
 #include "AdptArray.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct AdptArray_{
-    int size;
     int capacity;
     PElement* array;
     DEL_FUNC delete;
     COPY_FUNC copy;
     PRINT_FUNC print;
 } AdptArray;
-PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC delete,PRINT_FUNC print){
+PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC delete,PRINT_FUNC print)
+{
     PAdptArray ans = (PAdptArray)malloc(sizeof(AdptArray));
     if (ans == NULL){
         return NULL;
     }
     ans->array = (PElement*)malloc(sizeof(PElement));   
-    if (ans->array == NULL){
+    if (ans->array == NULL ){
         free(ans);
         return NULL;
     }   
     ans->delete = delete;
     ans->copy = copy;
     ans->print = print;
-    ans->size = 0;
     ans->capacity = 1;
     return ans;
 }
-Result SetAdptArrayAt (PAdptArray adptArray, int i, PElement element){
+
+Result SetAdptArrayAt (PAdptArray adptArray, int i, PElement element)
+{
     if (i < 0){
         return FAIL;
     }
     if (i >= adptArray->capacity){
         PElement* temp = (PElement*)realloc(adptArray->array, sizeof(PElement)*(i+1));
         if (temp == NULL){
+            DeleteAdptArray(adptArray);
             return FAIL;
-            free(adptArray);
         }
         adptArray->array = temp;
         adptArray->capacity = i+1;
         
     }
-    if (i > adptArray->size){
-            adptArray->size = i+1;
-    }
+    
     adptArray->array[i] = adptArray->copy(element);
     return SUCCESS;
 }
 
-PElement GetAdptArrayAt(PAdptArray adptArray, int i ){
-    if (i >= adptArray->size || i < 0 ){
+PElement GetAdptArrayAt(PAdptArray adptArray, int i )
+{
+    if (i >= adptArray->capacity || i < 0){
 
         return NULL;
     }
@@ -58,25 +59,30 @@ PElement GetAdptArrayAt(PAdptArray adptArray, int i ){
     return adptArray->copy(adptArray->array[i]);
 }
 
-int GetAdptArraySize(PAdptArray adptArray){
+int GetAdptArraySize(PAdptArray adptArray)
+{
     if(adptArray == NULL){
         return -1;
     }
-    return adptArray->size;
+    return adptArray->capacity;
 }
 
-void DeleteAdptArray(PAdptArray adptArray){
-    for (int i = 0; i < adptArray->size; i++){
+void DeleteAdptArray(PAdptArray adptArray)
+{
+    for (int i = 0; i < adptArray->capacity; i++){
         if (adptArray->array[i] != NULL){
+
             adptArray->delete(adptArray->array[i]);
         }
     }
     free(adptArray->array);
+
     free(adptArray);
 }
 
-void PrintDB(PAdptArray adptArray){
-    for (int i = 0; i < adptArray->size; i++){
+void PrintDB(PAdptArray adptArray)
+{
+    for (int i = 0; i < adptArray->capacity; i++){
         if(adptArray->array[i] != NULL){
             adptArray->print(adptArray->array[i]);
         }
